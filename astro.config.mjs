a3099d133f07222e5b7b6f96249112b37b28abd0
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2024-12-02 22:53:14 -0500

    vercel add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel(),
 });
